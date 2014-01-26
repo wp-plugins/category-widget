ebd75e376ef3809945ae61c31eca629cb8aa58a9
vinoth06 <vinoth06@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-26 03:32:20 +0000

    updated to 3.8.1
    git-svn-id: https://plugins.svn.wordpress.org/category-widget/tags/1.4.2.1.1@845506 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ category-widget.php
@@ -0,0 +1,173 @@
+<?php
+/*
+Plugin Name: Category Widget
+Plugin URI: http://buffercode.com/plugin-display-post-titles-category-selection/
+Description: Easy way to display the number of post in that particular category by selecting from admin dashboard widget.
+Version: 1.4.2.1.1
+Author: vinoth06
+Author URI: http://buffercode.com/
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Additing Action hook widgets_init
+add_action( 'widgets_init', 'buffercode_category_widget'); 
+
+function buffercode_category_widget() {
+register_widget( 'buffercode_category_widget_info' );
+}
+
+class buffercode_category_widget_info extends WP_Widget {
+function buffercode_category_widget_info () {
+		$this->WP_Widget('buffercode_category_widget_info', 'BC Category Widget','Select the category to display');	}
+
+public function form( $instance ) { 
+if ( isset( $instance[ 'buffercode_no_post' ]) && isset ($instance[ 'buffercode_category' ]) && isset ($instance[ 'buffercode_name' ])  && isset ($instance[ 'buffercode_bullet' ]) && isset ($instance[ 'buffercode_title_count' ]) && isset ($instance[ 'buffercode_combo_list' ]) &&isset ($instance[ 'buffercode_please_select'])) {
+			$buffercode_no_post_value = $instance[ 'buffercode_no_post' ];
+			$buffercode_category_value = $instance[ 'buffercode_category' ];
+			$buffercode_name_value = $instance[ 'buffercode_name' ];
+			$buffercode_bullet_value = $instance[ 'buffercode_bullet' ];
+			$buffercode_title_count_value = $instance[ 'buffercode_title_count' ];
+			$buffercode_combo_list_value = $instance[ 'buffercode_combo_list' ];		
+			$buffercode_please_select_value=$instance[ 'buffercode_please_select' ];		
+		}
+		else {//Setting Default Values
+		$buffercode_no_post_value = 5;
+		$buffercode_category_value = 1;
+		$buffercode_name_value	= 'My Category';
+		$buffercode_bullet_value = 2;
+		$buffercode_title_count_value = 20;
+		$buffercode_combo_list_value =1;
+		$buffercode_please_select_value='Please Select';
+		} ?><!-- Buffercode.com Category Widget Options -->
+		<p>Name: <input class="widefat" name="<?php echo $this->get_field_name( 'buffercode_name' ); ?>" type="text" value="<?php echo esc_attr( $buffercode_name_value );?>" /></p>
+		
+		<p>Select Category: <?php wp_dropdown_categories(array('name' => $this->get_field_name('buffercode_category'), 'selected' => $buffercode_category_value, 'id' => $this->get_field_id('buffercode_category'), 'class' => 'widefat')); ?> </p>
+		
+		<p>Number of Post:
+		<select name="<?php echo $this->get_field_name('buffercode_no_post'); ?>" id="<?php echo $this->get_field_id('buffercode_no_post'); ?>" class="widefat">
+		<?php
+		$options = array('1' =>'1','2' =>'2','3' =>'3','4' =>'4','5' =>'5','6'=>'6','7'=>'7','8'=>'8','9'=>'9','10'=>'10','All'=>'11');
+		foreach ($options as $langu => $code) {
+		echo '<option value="' . $code . '" id="' . $code . '"', $buffercode_no_post_value == $code ? ' selected="selected"' : '', '>', $langu, '</option>';
+		}
+		?>
+		</select></p>
+		
+		<p>Display in Combo box or List:
+		<select name="<?php echo $this->get_field_name('buffercode_combo_list'); ?>" id="<?php echo $this->get_field_id('buffercode_combo_list'); ?>" class="widefat">
+		<?php
+		$cl_options = array('Combo Box' =>'1','List' =>'2');
+		foreach ($cl_options as $cl_langu => $cl_code) {
+		echo '<option value="' . $cl_code . '" id="' . $cl_code . '"', $buffercode_combo_list_value == $cl_code ? ' selected="selected"' : '', '>', $cl_langu, '</option>';
+		}
+		?>
+		</select></p>
+		
+		<p>Show Bullet Style:
+		<select name="<?php echo $this->get_field_name('buffercode_bullet'); ?>" id="<?php echo $this->get_field_id('buffercode_bullet'); ?>" class="widefat">
+		<?php
+		$bullet_options = array('Yes' =>'1','No' =>'2');
+		foreach ($bullet_options as $bullet_value => $bullet_code) {
+		echo '<option value="' . $bullet_code . '" id="' . $bullet_code . '"', $buffercode_bullet_value == $bullet_code ? ' selected="selected"' : '', '>', $bullet_value, '</option>';
+		}
+		?>
+		</select></p>
+		
+		<p>Max Title Size:<input class="widefat" name="<?php echo $this->get_field_name( 'buffercode_title_count' ); ?>" type="text" value="<?php echo esc_attr( $buffercode_title_count_value );?>" /></p>
+		
+		<p>Custom Combo Box "Please Select":<input class="widefat" name="<?php echo $this->get_field_name( 'buffercode_please_select' ); ?>" type="text" value="<?php echo esc_attr( $buffercode_please_select_value );?>" /></p>
+		
+<?php
+}
+
+function update($new_instance, $old_instance) {
+$instance = $old_instance;
+$instance['buffercode_no_post'] = ( ! empty( $new_instance['buffercode_no_post'] ) ) ? strip_tags( $new_instance['buffercode_no_post'] ) : '';
+
+$instance['buffercode_category'] = ( ! empty( $new_instance['buffercode_category'] ) ) ? strip_tags( $new_instance['buffercode_category'] ) : '';
+
+$instance['buffercode_name'] = ( ! empty( $new_instance['buffercode_name'] ) ) ? strip_tags( $new_instance['buffercode_name'] ) : '';
+
+$instance['buffercode_bullet'] = ( ! empty( $new_instance['buffercode_bullet'] ) ) ? strip_tags( $new_instance['buffercode_bullet'] ) : '';
+
+$instance['buffercode_title_count'] = ( ! empty( $new_instance['buffercode_title_count'] ) ) ? strip_tags( $new_instance['buffercode_title_count'] ) : '';
+
+$instance['buffercode_combo_list'] = ( ! empty( $new_instance['buffercode_combo_list'] ) ) ? strip_tags( $new_instance['buffercode_combo_list'] ) : '';
+
+$instance['buffercode_please_select'] = ( ! empty( $new_instance['buffercode_please_select'] ) ) ? strip_tags( $new_instance['buffercode_please_select'] ) : '';
+return $instance;
+}
+
+function widget($args, $instance) {
+extract($args);
+echo $before_widget;
+$buffercode_name_value = apply_filters( 'widget_title', $instance['buffercode_name'] );
+$buffercode_category_value = empty( $instance['buffercode_category'] ) ? '&nbsp;' :
+$instance['buffercode_category'];
+$buffercode_no_post_value = empty( $instance['buffercode_no_post'] ) ? '&nbsp;' :
+$instance['buffercode_no_post'];
+$buffercode_bullet_value = empty( $instance['buffercode_bullet'] ) ? '&nbsp;' :
+$instance['buffercode_bullet'];
+$buffercode_title_count_value = empty( $instance['buffercode_title_count'] ) ? '&nbsp;' :
+$instance['buffercode_title_count'];
+$buffercode_combo_list_value = empty( $instance['buffercode_combo_list'] ) ? '&nbsp;' :
+$instance['buffercode_combo_list'];
+$buffercode_please_select_value = empty( $instance['buffercode_please_select'] ) ? 'Please Select' :
+$instance['buffercode_please_select'];
+
+if ( !empty( $name ) ) { echo $before_title . $buffercode_name_value .
+$after_title; };
+if($buffercode_no_post_value==11){$args = array('category' => $buffercode_category_value,'posts_per_page'   => -1 );}
+else{ $args = array('category' => $buffercode_category_value,'posts_per_page'   => $buffercode_no_post_value );}
+$myposts = get_posts( $args );
+global $post;
+if($buffercode_combo_list_value == 2){
+foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+	<li <?php if($buffercode_bullet_value==2){?> style="list-style-type:none;<?php } ?>"><!-- Buffercode.com Category Link Start -->
+		<a href="<?php the_permalink(); ?>"><?php $check_title=get_the_title(); $title_count=strlen($check_title);
+		if ($title_count>$buffercode_title_count_value){
+		$substr_title=substr($check_title, 0, $buffercode_title_count_value);
+		echo $substr_title.'...';
+		}else{echo $check_title;	}
+		?></a>
+		<!-- Buffercode.com Category Link Ends -->
+	</li>
+<?php endforeach;  } else {?>
+
+<select id="buffercode_option">
+<option value="" selected ><?php echo $buffercode_please_select_value; ?></option>
+<?php foreach ( $myposts as $post ) : setup_postdata( $post ); 
+		
+		$check_title=get_the_title();
+		$title_count=strlen($check_title);
+		if ($title_count>$buffercode_title_count_value){
+		$substr_title=substr($check_title, 0, $buffercode_title_count_value).'...';
+		}else{$substr_title=$check_title;	}
+		?>
+	<!-- Buffercode.com Category Link Start -->
+		<option value="<?php echo the_permalink(); ?>"><?php echo $substr_title ?></option>
+		<!-- Buffercode.com Category Link Ends -->
+	
+<?php endforeach; ?>
+</select>
+
+<?php
+}
+wp_reset_postdata();
+echo $after_widget;
+}
+}
+add_action( 'wp_head', 'buffercode_category_widget_script_new' );
+
+function buffercode_category_widget_script_new() {
+echo '<script>
+window.onload=function(){
+    var combo=document.getElementById(\'buffercode_option\');
+    combo.onchange=function(){
+        if(this.value!=\'0\') window.open(this.value, \'_self\');
+		};
+}; 
+</script>';
+}
+?>
